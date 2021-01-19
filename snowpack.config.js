bcd99f36fa9d9b7132f3de1948626c9edba6cb4c
hennessyevan <hennessyevan@users.noreply.github.com>
2021-01-19 12:55:31 -0500

    init

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,29 +1,24 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-babel',
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
+  routes: [],
   optimize: {
     /* Example: Bundle your final build: */
     // "bundle": true,
   },
   packageOptions: {
-    /* ... */
+    knownEntrypoints: ['react/jsx-runtime'],
   },
   devOptions: {
-    /* ... */
+    open: 'none',
   },
-  buildOptions: {
-    /* ... */
-  },
-};
+  buildOptions: {},
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Heading, Text, ChevronRightIcon, Spinner } from 'evergreen-ui'
+import { useEffect, useState } from 'react'
+import { useQuery, QueryClientProvider, QueryClient } from 'react-query'
 
 interface AppProps {}
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+const queryClient = new QueryClient()
+
+export function App({}: AppProps) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <GetAllRinks />
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+type RinkListEntry = {
+  objectid: number
+  facility_master_id: number
+  location: string
+  locationid: number
+  x: number
+  y: number
+  address: string
+  amenities: string
+  operational_hours: string
+  funguide_url?: string
+  globalid: string
+  reservations?: any
+  website: string
+}
+
+type AllRinksData = {
+  features: {
+    attributes: RinkListEntry
+  }[]
+}
+
+function GetAllRinks() {
+  const { data, isLoading } = useQuery<AllRinksData>('rinks', async () => {
+    return await fetch('/api/getAllRinks').then(r => {
+      return r.json()
+    })
+  })
+
+  return (
+    <div
+      style={{
+        padding: 16,
+      }}
+    >
+      <h1>Rinks</h1>
+      {isLoading ? (
+        <div
+          style={{
+            display: 'flex',
+            placeContent: 'center',
+            alignContent: 'center',
+            width: '100%',
+            height: '100vh',
+          }}
+        >
+          <Spinner />
+        </div>
+      ) : (
+        <Rinks rinks={data!.features.map(f => f.attributes)} />
+      )}
+    </div>
+  )
+}
+
+function Rinks({ rinks }: { rinks: RinkListEntry[] }) {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
+        gap: 16,
+      }}
+    >
+      {rinks.map(rink => (
+        <Card
+          border
+          padding={16}
+          key={rink.globalid}
+          display="flex"
+          placeItems="center"
+        >
+          <div>
+            <Heading paddingBottom={4}>{rink.location}</Heading>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <Text>{rink.address}</Text>
+              <Text>{rink.operational_hours}</Text>
+            </div>
+          </div>
+          <div style={{ marginLeft: 'auto' }}>
+            <ChevronRightIcon />
+          </div>
+        </Card>
+      ))}
+    </div>
+  )
+}
+
+function Rink({ id }: { id: string }) {
+  const [rinkData, setRinkData] = useState()
+
+  useEffect(() => {
+    ;(async function loadRinkData() {
+      const data = await fetch(`/api/getRinkInfo?id=${id}`).then(r => {
+        return r.json()
+      })
+
+      setRinkData(data)
+    })()
+  }, [])
+
+  return (
+    <div>
+      First Rink
+      <pre>{JSON.stringify(rinkData, null, 3)}</pre>
+    </div>
+  )
+}
